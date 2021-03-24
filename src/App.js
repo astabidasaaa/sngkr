f7f348457c2712ee845a2bcfd654fe06b7f1731b
astabidasaaa <astabidasabcd@gmail.com>
2021-03-24 19:06:19 +0700

    Building everything

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import video from "./assets/tv-static.mp4";
+import "./styles/main.scss";
 
 function App() {
+  const b = (1.3 * window.innerWidth * window.innerHeight) / (48 * 48);
+
+  const loop = (max) => {
+    let a = [];
+
+    for (let i = 0; i < max; i++) {
+      a.push(<div className="item" key={i}></div>);
+    }
+
+    return a;
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <video autoPlay muted loop id="background-video">
+        <source src={video} type="video/mp4" />
+      </video>
+      <div id="grid">{loop(b)}</div>
+      <div className="content">
+        <h1 title="HELLO, I'M SNGKR.">HELLO, I'M SNGKR.</h1>
+        <h3 title="reach me">reach me</h3>
+        <h3 title="+6281282096579">+6281282096579</h3>
+        <h3 title="sangkaranararya@gmail.com">sangkaranararya@gmail.com</h3>
+      </div>
     </div>
   );
 }
